e6470c8c07df34d82f52f432fc1ce4db9373f8db
Claudio Silva <claudio@solee.com.br>
2014-06-02 21:33:17 -0300

    DadosBasicos

--- php/usuario.php
+++ php/usuario.php
@@ -2,8 +2,8 @@
 $retorno = array(
     'usuario' => array(
         'nome' => "Claudio Antonio Silva Santos",
-        'email' => "claudio@solee.com.br",
-        'dataNascimento' =>  "09/02/1987",
+        'email' => "claudio@teste.com.br",
+        'dataNascimento' =>  "01/02/2001",
         'sexo' =>  "M",
         'estadoCivil' => array('codigo' => 2, 'descricao' => "Casado(a)"),
         'filhos' => '1',
@@ -13,13 +13,13 @@ $retorno = array(
         'pretensaoSalarial' =>  "10.000,00",
         'estado' =>  "DF",
         'cidade' =>  "Brasília",
-        'endereco' =>  "Rua 24 Norte",
-        'complemento' =>  "LT 9/11",
-        'bairro' =>  "Águas Claras",
-        'cep' =>  "71916-750",
-        'telefoneResidencial' =>  "61 3254 0699",
-        'telefoneComercial' =>  "61 81570669",
-        'telefoneCelular' =>  "61 91090039"
+        'endereco' =>  "Rua das Casas",
+        'complemento' =>  "LT 125",
+        'bairro' =>  "Águas do Norte",
+        'cep' =>  "70999-001",
+        'telefoneResidencial' =>  "61 33333333",
+        'telefoneComercial' =>  "61 99999999",
+        'telefoneCelular' =>  "61 90909090"
     )
 );
 
