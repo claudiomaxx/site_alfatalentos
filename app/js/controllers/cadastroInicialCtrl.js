fc776be2fadd0482e9ca08cce54c547d2a11431c
Claudio Silva <claudio@solee.com.br>
2014-06-03 21:14:14 -0300

    DadosBasicos

--- app/js/controllers/cadastroInicialCtrl.js
+++ app/js/controllers/cadastroInicialCtrl.js
@@ -2,6 +2,8 @@
 
 app.controller('cadastroInicialCtrl', ['$http', '$scope', '$location', 'usuarioService', function($http, $scope, $location, usuarioService){
     
+    $http.get('/site_alfatalentos/php/logout.php');
+    
     $scope.mensagem = "";
     $scope.tipoCadastro = 0;
     $scope.usuario = {
@@ -11,8 +13,22 @@ app.controller('cadastroInicialCtrl', ['$http', '$scope', '$location', 'usuarioS
     };
     
     $scope.cadastrar = function(){
+        $scope.mensagem = "";
+        
         if($scope.tipoCadastro < 1 || $scope.tipoCadastro > 2) {
             $scope.mensagem = "Tipo de cadastro não selecionado.";
+            
+        } else if($scope.usuario.nome === "") {
+            $scope.mensagem = "Nome não informado.";
+            
+        } else if($scope.usuario.email === "") {
+            $scope.mensagem = "Email não informado.";
+
+        } else if($scope.usuario.email.indexOf("@") < 1 || $scope.usuario.email.indexOf(".") < 3) {
+            $scope.mensagem = "Email inválido.";
+            
+        } else if($scope.usuario.senha === "") {
+            $scope.mensagem = "Senha não informada.";
             
         } else {
             var cadastro = {
@@ -24,7 +40,7 @@ app.controller('cadastroInicialCtrl', ['$http', '$scope', '$location', 'usuarioS
 
             $promise.then(function(retorno){
                 if(retorno.data.mensagem === '') {
-                    usuarioService.add(usuario);
+                    usuarioService.add($scope.usuario);
                     $location.path( "/dados-basicos" );
 
                 } else {
--- app/js/controllers/loginCtrl.js
+++ app/js/controllers/loginCtrl.js
@@ -1,19 +1,32 @@
 'use strict';
 
-app.controller('loginCtrl', ['$scope', '$location', 'loginService', function($scope, $location, loginService){
+app.controller('loginCtrl', ['$scope', '$location', '$http', function($scope, $location, $http){
     
     $scope.mensagem = "";
     
     $scope.login = function(usuario){
-        var $promise = loginService.login(usuario);
+        if($scope.usuario.email === "") {
+            $scope.mensagem = "Email não informado.";
+
+        } else if($scope.usuario.email.indexOf("@") < 1 || $scope.usuario.email.indexOf(".") < 3) {
+            $scope.mensagem = "Email inválido.";
+            
+        } else if($scope.usuario.senha === "") {
+            $scope.mensagem = "Senha não informada.";
+            
+        } else {
+            var $promise = $http.post('/site_alfatalentos/php/login.php', usuario);
+
+            $promise.then(function(retorno){
+                console.log(retorno.data);
+                if(retorno.data.mensagem === '' && retorno.data.usuario instanceof Object) {
+                    $location.path( "/dados-basicos" );
+
+                } else {
+                    $scope.mensagem = retorno.data.mensagem;
+                }
+            });
+        }
         
-        $promise.then(function(retorno){
-            if(retorno.data === 'success') {
-                $location.path( "/dados-basicos" );
-                
-            } else {
-                $scope.mensagem = "Usuário/Senha inválidos.";
-            }
-        });
     };
 }]);
